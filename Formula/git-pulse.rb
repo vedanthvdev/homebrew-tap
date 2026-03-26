521c5a393917c01fa0d871a03e0a2a5ae3e9807e
vedanthvasudev <vedanth.vasudev@modulrfinance.com>
2026-03-26 21:46:39 +0000

    Simplify formula: use pip install with wheels

--- Formula/git-pulse.rb
+++ Formula/git-pulse.rb
@@ -9,68 +9,12 @@ class GitPulse < Formula
 
   depends_on "python@3.12"
 
-  resource "typer" do
-    url "https://files.pythonhosted.org/packages/source/t/typer/typer-0.24.1.tar.gz"
-    sha256 "e39b4732d65fbdcde189ae76cf7cd48aeae72919dea1fdfc16593be016256b45"
-  end
-
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/source/c/click/click-8.3.1.tar.gz"
-    sha256 "12ff4785d337a1bb490bb7e9c2b1ee5da3112e94a8622f26a6c77f5d2fc6842a"
-  end
-
-  resource "rich" do
-    url "https://files.pythonhosted.org/packages/source/r/rich/rich-14.3.3.tar.gz"
-    sha256 "b8daa0b9e4eef54dd8cf7c86c03713f53241884e814f4e2f5fb342fe520f639b"
-  end
-
-  resource "pyyaml" do
-    url "https://files.pythonhosted.org/packages/source/P/PyYAML/pyyaml-6.0.3.tar.gz"
-    sha256 "d76623373421df22fb4cf8817020cbb7ef15c725b9d5e45f17e189bfc384190f"
-  end
-
-  resource "gitpython" do
-    url "https://files.pythonhosted.org/packages/source/G/GitPython/gitpython-3.1.46.tar.gz"
-    sha256 "400124c7d0ef4ea03f7310ac2fbf7151e09ff97f2a3288d64a440c584a29c37f"
-  end
-
-  resource "gitdb" do
-    url "https://files.pythonhosted.org/packages/source/g/gitdb/gitdb-4.0.12.tar.gz"
-    sha256 "5ef71f855d191a3326fcfbc0d5da835f26b13fbcba60c32c21091c349ffdb571"
-  end
-
-  resource "smmap" do
-    url "https://files.pythonhosted.org/packages/source/s/smmap/smmap-5.0.3.tar.gz"
-    sha256 "4d9debb8b99007ae47165abc08670bd74cb74b5227dda7f643eccc4e9eb5642c"
-  end
-
-  resource "markdown-it-py" do
-    url "https://files.pythonhosted.org/packages/source/m/markdown-it-py/markdown_it_py-4.0.0.tar.gz"
-    sha256 "cb0a2b4aa34f932c007117b194e945bd74e0ec24133ceb5bac59009cda1cb9f3"
-  end
-
-  resource "mdurl" do
-    url "https://files.pythonhosted.org/packages/source/m/mdurl/mdurl-0.1.2.tar.gz"
-    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
-  end
-
-  resource "pygments" do
-    url "https://files.pythonhosted.org/packages/source/P/Pygments/pygments-2.19.2.tar.gz"
-    sha256 "636cb2477cec7f8952536970bc533bc43743542f70392ae026374600add5b887"
-  end
-
-  resource "shellingham" do
-    url "https://files.pythonhosted.org/packages/source/s/shellingham/shellingham-1.5.4.tar.gz"
-    sha256 "8dbca0739d487e5bd35ab3ca4b36e11c4078f3a234bfce294b0a0291363404de"
-  end
-
-  resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/source/t/typing_extensions/typing_extensions-4.15.0.tar.gz"
-    sha256 "0cea48d173cc12fa28ecabc3b837ea3cf6f38c6d1136f85cbaaf598984861466"
-  end
-
   def install
-    virtualenv_install_with_resources
+    virtualenv_create(libexec, "python3.12")
+    system libexec/"bin/pip", "install", "--no-cache-dir",
+           "typer>=0.9", "rich>=13", "pyyaml>=6", "gitpython>=3.1", "shellingham>=1.5"
+    system libexec/"bin/pip", "install", "--no-cache-dir", "--no-deps", buildpath
+    bin.install_symlink Dir[libexec/"bin/git-pulse"]
   end
 
   test do
